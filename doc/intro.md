bbe34b2ffff0ac516551dfec7b43f13444f33439
wuzhenben <wzb@wuzhenben.com>
2024-07-27 21:15:59 +0800

    chore: created a new project by running command 'lein new app wuzhenbens-clojure-playground'

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to wuzhenbens-clojure-playground
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/wuzhenbens_clojure_playground/core_test.clj
@@ -0,0 +1,7 @@
+(ns wuzhenbens-clojure-playground.core-test
+  (:require [clojure.test :refer :all]
+            [wuzhenbens-clojure-playground.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
